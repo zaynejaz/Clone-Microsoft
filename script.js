8a2b6d1e4fc8b518ea34c9cf85f12c491e22ed52
zejaz <zayn.ejaz@gmail.com>
2020-03-24 23:14:27 +0300

    adj navbar + added tooltip

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 document.querySelector('.menu-btn').addEventListener('click', () => document.querySelector('.main-menu').classList.toggle('show'))
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+  });
